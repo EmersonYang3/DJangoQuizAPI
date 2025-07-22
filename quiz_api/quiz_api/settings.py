e69ee2c1fed86fa458bf3e6e2f0c310a9d1a54ee
EmersonYang3 <emersonyang28@gmail.com>
2025-07-22 09:08:35 -0400

    Add CORS support with django-cors-headers
    Added 'corsheaders' to INSTALLED_APPS and 'corsheaders.middleware.CorsMiddleware' to MIDDLEWARE to enable Cross-Origin Resource Sharing (CORS) in the Django project.

--- quiz_api/quiz_api/settings.py
+++ quiz_api/quiz_api/settings.py
@@ -35,6 +35,7 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
